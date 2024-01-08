d03433f99716a94124128610e62ec93ba04b6cf4
KrzysztofGarus <krzysztof.garus@gmail.com>
2024-01-08 14:53:40 +0100

    Initial commit

---
+++ src/main/java/pl/someday/FishingAppRestApi/FishingAppRestApiApplication.java
@@ -0,0 +1,13 @@
+package pl.someday.FishingAppRestApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FishingAppRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FishingAppRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/someday/FishingAppRestApi/FishingAppRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.someday.FishingAppRestApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FishingAppRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
