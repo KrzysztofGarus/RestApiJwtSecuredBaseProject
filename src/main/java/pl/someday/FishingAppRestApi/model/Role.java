bbbe9d0d5ff5c6b8ffdf5c91b48760dfb245a9e0
KrzysztofGarus <krzysztof.garus@gmail.com>
2024-01-08 15:04:32 +0100

    added user entity and user repo

---
+++ src/main/java/pl/someday/FishingAppRestApi/model/Role.java
@@ -0,0 +1,6 @@
+package pl.someday.FishingAppRestApi.model;
+
+public enum Role {
+    USER,
+    ADMIN
+}
---
+++ src/main/java/pl/someday/FishingAppRestApi/model/User.java
@@ -0,0 +1,24 @@
+package pl.someday.FishingAppRestApi.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    private String password;
+
+    private Role role;
+
+}
