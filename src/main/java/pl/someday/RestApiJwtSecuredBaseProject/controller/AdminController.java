2e7357ebeb33d232e69ca802fae80025ef93ccf1
Krzysztof Garus <krzysztof.garus@gmail.com>
2024-01-15 16:05:49 +0100

    added admin controller endpoint

--- src/main/java/pl/someday/RestApiJwtSecuredBaseProject/controller/AdminController.java
+++ src/main/java/pl/someday/RestApiJwtSecuredBaseProject/controller/AdminController.java
@@ -1,11 +1,19 @@
 package pl.someday.RestApiJwtSecuredBaseProject.controller;
 
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/api/admin")
 @RequiredArgsConstructor
 public class AdminController {
+
+    @GetMapping("/hello")
+    public ResponseEntity<String> hello(){
+        return ResponseEntity.ok("Hello admin!");
+    }
 }
