50b5c732815512fa3bbcf6a55f6283d4113bcb2e
Krzysztof Garus <krzysztof.garus@gmail.com>
2024-01-15 13:19:46 +0100

    renamed project

--- src/main/java/pl/someday/RestApiJwtSecuredBaseProject/RestApiJwtSecuredBaseProject.java
+++ src/main/java/pl/someday/RestApiJwtSecuredBaseProject/RestApiJwtSecuredBaseProject.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FishingAppRestApiApplication {
+public class RestApiJwtSecuredBaseProject {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FishingAppRestApiApplication.class, args);
+		SpringApplication.run(RestApiJwtSecuredBaseProject.class, args);
 	}
 
 }
